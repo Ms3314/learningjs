f08d4938e189854d09cfd026e1dbb34699ae992a
Mohammed Smiuddin <66519042+Ms3314@users.noreply.github.com>
2024-03-30 07:33:06 +0000

    jinger boo

--- foreach.js
+++ foreach.js
@@ -1,4 +1,37 @@
 const lang = ["cpp","js","python","ruby"]
 lang.forEach((item)=>{
+    //console.log(item);
+})
+
+function printME(item){
     console.log(item);
+}
+
+// lang.forEach(printME)
+// how for each exactly takes and all 
+lang.forEach((item , index , arr)=>{
+    // console.log(item , index , arr);
+})
+
+const myCooding = [
+    {
+        langname : "Javrkascript",
+        langfile : "jrs"
+        
+    },
+    {
+        langname : "Javkascript",
+        langfile : "jk"
+
+    },
+
+    {
+        langname : "Jakvakscript",
+        langfile : "jhs"
+
+    },
+]
+
+myCooding.forEach((item)=>{
+    console.log(item.langname);
 })
---
+++ loopy.js
@@ -0,0 +1,1 @@
+const cooding = [2,3,4,5,6]
